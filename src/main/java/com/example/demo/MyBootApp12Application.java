cae96a2933acba7d87b4b47ebbd7a63ba82853cf
panka <panka@192.168.11.3>
2020-10-06 08:53:30 +0900

    Set

---
+++ src/main/java/com/example/demo/MyBootApp12Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBootApp12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBootApp12Application.class, args);
+	}
+
+}
